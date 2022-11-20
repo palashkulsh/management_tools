37bd232195a1b64551b74b34ca38e677476c43e2
palash <palash.kulshreshtha@paytm.com>
2022-11-20 22:00:55 +0530

    fixed bug

--- option_generator.py
+++ option_generator.py
@@ -15,6 +15,7 @@ merge_format = workbook.add_format({
     'border': 1,
     'align': 'center',
     'valign': 'vcenter',
+    'text_wrap': 1
     })
 
 
@@ -32,6 +33,12 @@ inputs = {
     "e":["e1",'e2','e3','e4']
 }
 
+inputs = {
+    "os": ["u18 patched","u20"],
+    "nginx": ["1.13","1.19","1.21"],
+    "nodejs": ["8.9.4","long term stable"]
+}
+
 total_columns = len(inputs)
 total_rows = 1
 ordered_keys = list(inputs.keys())
@@ -39,8 +46,12 @@ ordered_sizes = list(map(lambda key: len(inputs[key]), ordered_keys))
 total_rows = reduce(lambda x,y: x*y,ordered_sizes)
 
 #this solution is limited to 26 options only
+
+# traverse all keys in input
 for index in range(len(ordered_keys)):
     option = ordered_keys[index]
+    #decide how many cells to merge
+    #vertically each cell is merge of product of number of options below it
     if index==len(ordered_sizes)-1:
         curr_level_merge_size=1
     else:
@@ -49,18 +60,29 @@ for index in range(len(ordered_keys)):
 
     print(curr_level_merge_size)
     current_column = chr(ord('A')+index)
+    #leaving 1 row for header
+    worksheet.write(0, index, ordered_keys[index], merge_format)
+    #for merged cells take indexing from 1, so starting from second row
     start_row=2
     print(current_column)
-    while start_row<total_rows:
+    #repeat till total_rows are filled with repetition
+    while start_row<=total_rows:
+        #repeat on each element of a given option
         for item in range(ordered_sizes[index]):
             print(inputs[option][item])
+            #generate ranges to merge
+            #end the merge at 1 row less than the sum
             range_to_merge='{}{}:{}{}'.format(current_column,start_row,current_column,start_row+curr_level_merge_size-1)
             print(range_to_merge)
+            #merge is only required for other than last option
             if(curr_level_merge_size>1):
                 worksheet.merge_range(range_to_merge,inputs[option][item] , merge_format)
             else:
+                #for last option cells are not merged, values for last option are just repeated
+                print('total_row {} start_row {}'.format(total_rows,start_row))
                 #because in write function rows and columns are zero indexed
-                worksheet.write(start_row-1, index,inputs[option][item])
+                #so start row is decremented by 1
+                worksheet.write(start_row-1, index,inputs[option][item], merge_format)
 
             start_row=start_row+curr_level_merge_size
 
